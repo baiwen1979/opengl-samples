ee3ab8f62aeb079f52dd4e1ba7ea511e6beae747
baiwen1979 <baiwen1979@qq.com>
2018-03-22 13:24:14 +0800

    更新

--- src/cg_main.cpp
+++ src/cg_main.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc,char *argv[]) {
     // 测试2D图元生成算法
-    //testPrimitive2d();
-    // 测试课程实验
+    testPrimitive2d();
+    // 测试图形学课程实验
     //testCGCourseLab();
     // 测试OpenGL教程
     //testOGLTutorial();
